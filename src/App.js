defae7c529ed29b673f3ea5e817c1656fc578e9d
kaungkhantthar7777 <kaungkhantthar@ucsm.edu.mm>
2020-05-04 20:26:51 +0630

    After adding slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Palette from './Palette'
 import seedColors from './seedColors'
+import { generatePalette } from './colorsHelper';
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[5]} />
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
+
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './Palette.css';
-
-
 export default class Palette extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            level : 500
+        }
+        this.handleSlide = this.handleSlide.bind(this);
+    }
+    handleSlide(level){
+        console.log(level);
+        this.setState({
+            level
+        })
+    }
     render() {
-        const colors = this.props.colors.map(colorObj => <ColorBox background={colorObj.color} name={colorObj.name} />)
+        const { level } = this.state;
+        const colors = this.props.palette.colors[level].map(colorObj => <ColorBox background={colorObj.hex} name={colorObj.name} />)
         return (
             <div className="Palette">
                 {/* Nav here */}
+                <div className="slider">
+                    <Slider min={100} max={900} defaultValue={level} step={100} onAfterChange={this.handleSlide}/>
+                </div>
+                
                 <div className="Palette-colors">
                     {
                         colors
